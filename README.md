c9830838c35a112ffe29acc1bc5970fdab93782f
halangbacca <halanbacca@outlook.com>
2024-03-28 12:34:47 -0300

    Resolução do desafio Processando e Transformando Dados com Power BI

---
+++ README.md
@@ -0,0 +1,4 @@
+# Processando e Transformando Dados com Power BI
+- Instalar o MySQL
+- Executar o arquivo "script_bd_company.sql"
+- Executar o arquivo "insert_data.sql"
---
+++ script_bd_company.sql
@@ -0,0 +1,65 @@
+create schema if not exists azure_company;
+use azure_company;
+
+select * from information_schema.table_constraints
+	where constraint_schema = 'azure_company';
+
+CREATE TABLE employee(
+	Fname varchar(15) not null,
+    Minit char,
+    Lname varchar(15) not null,
+    Ssn char(9) not null, 
+    Bdate date,
+    Address varchar(30),
+    Sex char,
+    Salary decimal(10,2),
+    Super_ssn char(9),
+    Dno int not null
+);
+
+alter table employee modify Dno int not null default 1;
+
+desc employee;
+
+create table department(
+	Dname varchar(15) not null,
+    Dnumber int not null,
+    Mgr_ssn char(9) not null,
+    Mgr_start_date date, 
+    Dept_create_date date
+);
+
+desc department;
+
+create table dept_locations(
+	Dnumber int not null,
+	Dlocation varchar(15) not null
+);
+
+create table project(
+	Pname varchar(15) not null,
+	Pnumber int not null,
+    Plocation varchar(15),
+    Dnum int not null,
+    primary key (Pnumber)
+);
+
+create table works_on(
+	Essn char(9) not null,
+    Pno int not null,
+    Hours decimal(3,1) not null,
+    primary key (Essn, Pno)
+);
+
+create table dependent(
+	Essn char(9) not null,
+    Dependent_name varchar(15) not null,
+    Sex char,
+    Bdate date,
+    Relationship varchar(8),
+    primary key (Essn, Dependent_name)
+);
+
+desc dependent;
+
+show tables;
